989bc9ec56e922260bbb9e05c79acfb6c73f86c6
Molnar Kristian <molnarkristian@Molnars-MacBook-Air.local>
2017-04-18 11:56:39 +0300

    changed appdelegate

--- examleProj/AppDelegate.swift
+++ examleProj/AppDelegate.swift
@@ -11,8 +11,9 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-  var window: UIWindow?
-
+  var window: UIWindow? 
+//sasdkjhasdkjashdkjasdh
+  
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
