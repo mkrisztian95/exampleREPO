c4db3ad987861e7823ca05bc2afc7ea47832f1c7
Molnar Kristian <molnarkristian@Molnars-MacBook-Air.local>
2017-04-18 11:49:58 +0300

    Initial Commit

---
+++ examleProj/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  examleProj
+//
+//  Created by Molnar Kristian on 4/18/17.
+//  Copyright © 2017 Molnar Kristian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
